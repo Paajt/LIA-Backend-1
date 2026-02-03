6ad870682e4e04ae22eac95de144df9d5422d88e
Patrik <pat_p3@hotmail.com>
2026-02-03 18:21:04 +0100

    Add API testing in browser

--- nodejs-express/src/index.ts
+++ nodejs-express/src/index.ts
@@ -2,8 +2,15 @@ import express, { Request, Response } from 'express';
 import { fetchJoke } from './jokesApi.js';
 import { config } from './config.js';
 import { verifyToken } from './auth.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/status', (req: Request, res: Response) => {
 	res.json({ status: 'OK' });
@@ -65,4 +72,7 @@ app.get(
 
 app.listen(config.port, () => {
 	console.log(`Server running on http://localhost:${config.port}`);
+	console.log(
+		`Visit test.html here: http://localhost:${config.port}/test.html`
+	);
 });
