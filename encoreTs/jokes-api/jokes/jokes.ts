576a8c883a98e5981338e86672c56b76e63e66d8
Patrik <pat_p3@hotmail.com>
2026-02-04 13:30:38 +0100

    Add basic joke endpoint with JokesAPI integration

---
+++ encoreTs/jokes-api/jokes/jokes.ts
@@ -0,0 +1,35 @@
+import { api } from 'encore.dev/api';
+
+type JokeApiResponse =
+	| {
+			type: 'single';
+			joke: string;
+	  }
+	| {
+			type: 'twopart';
+			setup: string;
+			delivery: string;
+	  };
+
+type JokeResponse = {
+	joke: string[];
+};
+
+export const joke = api(
+	{ method: 'GET', path: '/jokes' },
+	async (): Promise<JokeResponse> => {
+		const response = await fetch('https://v2.jokeapi.dev/joke/any');
+
+		if (!response.ok) {
+			throw new Error('Failed to fetch joke!');
+		}
+
+		const data = (await response.json()) as JokeApiResponse;
+
+		if (data.type === 'single') {
+			return { joke: [data.joke] };
+		}
+
+		return { joke: [data.setup, data.delivery] };
+	}
+);
